a563446b845644fcb6899623cd162b773e14bbb9
Robert Bhanu Prasad <robertbhanuprasad@example.com>
2024-11-24 18:56:07 +0530

    feature:updated the tailwind config file

--- rbac/tailwind.config.js
+++ rbac/tailwind.config.js
@@ -15,6 +15,27 @@ theme: {
     },
     colors: {}
   }
-},
-plugins: [require("tailwindcss-animate")],
+}
+};
+
+import tailwindcssAnimate from "tailwindcss-animate";
+
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)'
+      },
+      colors: {}
+    }
+  },
+  plugins: [tailwindcssAnimate],
 };
